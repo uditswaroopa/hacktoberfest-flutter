f62ef9ec8bd3aabfd645b6986ca205a248c21127
anjaliswaroopa <72921007+anjaliswaroopa@users.noreply.github.com>
2020-10-15 17:06:13 +0530

    Update contributors_card_model.dart

--- lib/models/contributors_card_model.dart
+++ lib/models/contributors_card_model.dart
@@ -7,4 +7,33 @@ class ContributorCard {
   final String displayImgUrl;
   final String website;
   ContributorCard({@required this.userName,@required this.displayImgUrl,@required this.desc,@required this.website});
+  
+  isNameValid(){
+    if(username.length > 5)
+      return true;
+    else
+      return false;
+  }
+  
+  isdescValid(){
+    if(desc.length > 1)
+      return true;
+    else
+      return false;
+  }
+  
+  isImageValid(){
+    if(displayImgUrl.length > 20)
+      return true;
+    else
+      return false;
+  }
+  
+  iswebsiteValid(){
+    if(website.length > 20)
+      return true;
+    else
+      return false;
+  }
+  
 }
